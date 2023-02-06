b1e8357241b7247964ea9c652c82a04ce6ad15fb
DmityFilchev <filchev_dmitry@mail.ru>
2023-02-06 16:24:05 +0300

    Добавьте файлы проекта.

---
+++ Task28_1/Task28_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include<thread>
+#include<string>
+#include<mutex>
+
+class Swimmer
+{
+private:
+	std::string name;
+	double speed = 0;
+public:
+	void setName(std::string& inName)
+	{
+		name = inName;
+	}
+	std::string getName()
+	{
+		return name;
+	}
+	void setSpeed(double& inSpeed)
+	{
+		speed = inSpeed;
+	}
+	double getSpeed()
+	{
+		return speed;
+	}
+};
+
+int main()
+{
+	return 0;
+}
