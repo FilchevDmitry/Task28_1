a1950136af05faa1975d38dab4d64d424d526b59
DmityFilchev <filchev_dmitry@mail.ru>
2023-02-08 15:03:58 +0300

    '08_02'

--- Task28_1/Task28_1.cpp
+++ Task28_1/Task28_1.cpp
@@ -2,32 +2,85 @@
 #include<thread>
 #include<string>
 #include<mutex>
+#include<chrono>
+#include<vector>
+#include<map>
 
+std::mutex r;
 class Swimmer
 {
 private:
 	std::string name;
 	double speed = 0;
-public:
-	void setName(std::string& inName)
+public:	void setName()
 	{
-		name = inName;
+		std::cout<<"Imput name - ";
+		std::cin >> name;
 	}
 	std::string getName()
 	{
 		return name;
 	}
-	void setSpeed(double& inSpeed)
+	void setSpeed()
 	{
-		speed = inSpeed;
+		std::cout << "Imput speed (m/s) - ";
+		std::cin >> speed;
 	}
 	double getSpeed()
 	{
 		return speed;
 	}
+	void printDistance(double inDistanse)
+	{
+		double distanse = 0;
+		int temp = 0;
+		while (distanse < inDistanse)
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			temp++;
+			distanse = speed * temp;
+			if (distanse > inDistanse) distanse = inDistanse;
+			r.lock();
+			std::cout << "A swimmer " << name << " sailed : " << distanse << std::endl;
+			r.unlock();
+		}
+	}
+	double getTimeDistance(double &inDistance)
+	{
+		return inDistance / speed;
+	}
 };
 
 int main()
-{
+{	double dist = 100;
+	const int numberSwimmers = 6;
+	std::vector<Swimmer> swimmer;
+	std::map<double, std::string> table;
+	
+	for (int i = 0; i < numberSwimmers; i++)
+	{
+		swimmer.push_back(Swimmer());
+		swimmer[i].setName();
+		swimmer[i].setSpeed();
+	}
+	std::vector<std::thread> temp;
+	for (int i = 0; i < numberSwimmers; i++)
+	{	
+		temp.emplace_back(&Swimmer::printDistance,swimmer[i],dist);
+	}
+	for (auto& i : temp)
+	{
+		i.join();
+	}
+	for (int i = 0; i < numberSwimmers; i++)
+	{
+		table[swimmer[i].getTimeDistance(dist)] = swimmer[i].getName();
+	}
+	std::map<double, std::string>::iterator tab = table.begin();
+	std::cout << "Final table" << std::endl;
+	for (int i = 0; tab != table.end(); tab++, i++)
+	{
+		std::cout << tab->first << "  " << tab->second << std::endl;
+	}
 	return 0;
 }
